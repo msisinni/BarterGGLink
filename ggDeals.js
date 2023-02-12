d94b7ad9a120007ff0e738f54dabfcf624e45c7d
Matt Sisinni <matthewsisinni@gmail.com>
2023-02-12 10:11:29 -0500

    Initial commit

---
+++ ggDeals.js
@@ -0,0 +1,12 @@
+var priceLinks = document.querySelector("[data-test]");
+
+if (typeof(priceLinks) != 'undefined' && priceLinks != null)
+{
+  priceLinks.append(", ");
+  var text = document.createTextNode("gg.Deals");
+  var link = document.createElement("a");
+  link.appendChild(text);
+  var steamID = priceLinks.children[0].href.split('/')[4];
+  link.setAttribute("href", "https://gg.deals/steam/app/" + steamID + "/");
+  priceLinks.appendChild(link);
+}
